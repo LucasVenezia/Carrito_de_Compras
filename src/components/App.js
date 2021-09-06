df7d949e14863a2f302220a72f50778e8e086570
Lucas Venezia <lucasavenezia@gmail.com>
2021-09-06 17:47:59 -0300

    se crea navBar

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Nav from "./NavBar";
+import "./styles/titulo.css";
+
+const App = () => {
+    return (
+        <header>    
+            <h1 className="title">
+                FERRETERIA ALVEAR
+            </h1> 
+            <Nav/>
+        </header>  
+    )
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import "./styles/navBar.css"
+
+const Nav = () => {
+    return (
+            <nav className="menu">
+                <a className="nav-enlace" href="#">Home</a>
+                <a className="nav-enlace" href="#">Productos</a>
+                <a className="nav-enlace" href="#">Preguntas Frecuentes</a>
+                <a className="nav-enlace" href="#">Contacto</a>
+            </nav> 
+    )
+}
+
+export default Nav
+
